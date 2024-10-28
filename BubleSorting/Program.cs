a890f4104eee831eb863d9aaa8b332e0516c3e13
Siarhei Biadniuk <sergeybednyuk@gmail.com>
2024-10-28 16:45:01 +0100

    add buble sorting example

---
+++ BubleSorting/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace BubleSorting;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var case1 = new int[] { 29, 10, 14, 37, 13 };
+        var result = BubleSort(case1);
+        foreach (var item in result)
+        {
+            Console.WriteLine(item);
+        }
+    }
+
+    static IEnumerable<int> BubleSort(IList<int> numbers)
+    {
+        for (int i = 0; i < numbers.Count; i++)
+        {
+            for (int j = 1; j < numbers.Count - i; j++)
+            {
+                if (numbers[j] < numbers[j - 1])
+                {
+                    var tmp = numbers[j];
+                    numbers[j] = numbers[j - 1];
+                    numbers[j - 1] = tmp;
+                }
+            }
+        }
+        return numbers;
+    }
+}
