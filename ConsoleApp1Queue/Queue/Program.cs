c54e87063fb5546d2ee237785fde6e757f3393d4
Siarhei Biadniuk <sergeybednyuk@gmail.com>
2024-10-28 08:23:38 +0100

    Add Queue example

---
+++ ConsoleApp1Queue/Queue/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ConsoleApp1Queue;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Queue<int> queue = new Queue<int>();
+        queue.Enqueue(2); // 2
+        queue.Enqueue(3); // 2, 3
+        Console.WriteLine(queue.Peek()); // 2
+        queue.Dequeue(); // 3
+        Console.WriteLine(queue.Peek()); // 3
+        queue.Enqueue(4); // 3, 4
+        queue.Enqueue(5); // 3, 4, 5
+        queue.Enqueue(6); // 3, 4, 5, 6
+        queue.Dequeue();
+        Console.WriteLine(queue.Peek()); // 6
+        for (int i = 0; i < queue.Count; i++)
+        {
+            Console.WriteLine(queue.Peek());
+            queue.Dequeue();
+        }
+        Console.ReadKey();
+    }
+}
